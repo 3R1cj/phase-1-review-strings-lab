5e824f819b8b419555f5ad978a3aa62b35c55e29
Atuhaire Eric Jonadab <eric.atuhaire@student.moringaschool.com>
2024-07-06 15:36:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'Jonadab Eric'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` 
+
+
+
+
